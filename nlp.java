b25179c54a402861409f61db6a1bac80301f31f8
PILean <trinidad.leandro@protonmail.com>
2019-03-23 12:23:16 -0600

    added: lematization, stopwords, cleanText

--- nlp.java
+++ nlp.java
@@ -2,9 +2,13 @@ import java.util.*;
 import java.io.*;
 public class nlp {
 	public static void main(String[] arguments) {
-		String text = "this is the text that im going to use in the list";
-		List<String> stext = tokenize(text);
-		for(String test: stopwords(stext)) {
+		String text = "My name is Leandro and i love to go running when it is warm outside.";
+		String clean = cleanText(text);
+		List<String> token = tokenize(clean);
+		List<String> stopW = stopwords(token);
+		List<String> lemm = lemmatize(stopW);
+		for(String words: lemm) {
+			System.out.println(words);
 		}
 	}
 
@@ -18,41 +22,38 @@ public class nlp {
 
 	/* Remove stop words */
 	private static List<String> stopwords(List<String> text) {
-		/*
-		 * TODO: this needs to be done
-		 *
-		 * remove words that are common in the text like (the, is, etc)
-		 *
-		 *
-		 *
-		 *
-		 *
-		 * */
+		List<String> result = new ArrayList<String>();
+		List<String> stopwords = FileManager.openFile("data/stopwords-en.txt");
+		for(String word: text) {
+			if(!stopwords.contains(word.toLowerCase())) {
+				result.add(word);
+			}	
+		}
+		return result;
+	}
+	
+	/* Clean the text of symbols or non letters*/
+	private static String cleanText(String text) {
+		return text.replaceAll("[^a-zA-Z ]", "").toLowerCase();	
 	}
 
 
 	
 	/* group inflected forms of a word to a single item */
-	private static String lemmatize(String word) {
+	private static List<String> lemmatize(List<String> text) {
 		List<String> lema = FileManager.openFile("data/lemmatization-en.txt");	
 
-		for(String line: lema) {
-			String[] items = line.split("\t");
-
-			String root = items[0];
-			String branch = items[1];
-
-
-			// if word is a branch return root, if word is root return root
-			if(branch.toLowerCase().equals(word.toLowerCase())) {
-				return root;	
-			}	
-			else if(root.toLowerCase().equals(word.toLowerCase())) {
-				return root;	
+		for(int i = 0; i < text.size(); i++) {	
+			for(String line: lema) {
+				String[] items = line.split("\t");
+				String root = items[0];
+				String branch = items[1];
+				if(text.get(i).toLowerCase().equals(branch.toLowerCase())) {
+					text.set(i, root.toLowerCase());
+				}
 			}
-		}
-		
-		return null;
+		}		
+		return text;
 	}
 
 
