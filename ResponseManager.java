e880e97160f3527bc3d485ae8b75084d9964c8cc
PILean <trinidad.leandro@protonmail.com>
2019-03-23 15:00:57 -0600

    created for managing the responses

---
+++ ResponseManager.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+
+public class ResponseManager {
+
+	/* is a word in a List */
+	public static boolean isKey(List<String> text, String key) {
+		if(text.contains(key.toLowerCase())) {
+			return true;	
+		}	
+
+		return false;
+	}
+
+	/* Bot response */
+	public static String getResponse(List<String> text) {	
+		String filename = "data/response-en.txt";
+		List<String> oFile = FileManager.openFile(filename);
+		for(String line: oFile) {
+			String[] lineSplit = line.split("\t");
+			String key = lineSplit[0];
+			String response = lineSplit[1];
+			
+			if(text.contains(key.toLowerCase())) {
+				return response;	
+			}
+		}
+		return randomResponse();
+	}
+
+
+	/* Gets a random string from a list of responses*/
+	public static String randomResponse() {
+		String filename = "data/random.txt";
+		return FileManager.randChoice(FileManager.openFile(filename));	
+	}
+}
