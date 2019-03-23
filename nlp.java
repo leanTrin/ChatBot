2df3bf2760c78e14cf3aeec4e7816dbe6d935de5
PILean <trinidad.leandro@protonmail.com>
2019-03-23 15:01:43 -0600

    fixed the removal of symbols and added ignoring of numbers

--- nlp.java
+++ nlp.java
@@ -15,13 +15,13 @@ public class nlp {
 
 
 	/* Tokenize a string */
-	private static List<String> tokenize(String text) {
+	public static List<String> tokenize(String text) {
 		String[] split = text.split(" ");
 		return Arrays.asList(split);
 	}
 
 	/* Remove stop words */
-	private static List<String> stopwords(List<String> text) {
+	public static List<String> stopwords(List<String> text) {
 		List<String> result = new ArrayList<String>();
 		List<String> stopwords = FileManager.openFile("data/stopwords-en.txt");
 		for(String word: text) {
@@ -33,14 +33,14 @@ public class nlp {
 	}
 	
 	/* Clean the text of symbols or non letters*/
-	private static String cleanText(String text) {
-		return text.replaceAll("[^a-zA-Z ]", "").toLowerCase();	
+	public static String cleanText(String text) {
+		return text.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();	
 	}
 
 
 	
 	/* group inflected forms of a word to a single item */
-	private static List<String> lemmatize(List<String> text) {
+	public static List<String> lemmatize(List<String> text) {
 		List<String> lema = FileManager.openFile("data/lemmatization-en.txt");	
 
 		for(int i = 0; i < text.size(); i++) {	
